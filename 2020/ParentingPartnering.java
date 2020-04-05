3069efa9e20b547e6b757c18172a88e9920360ad
Alvina Putri <av_ashfan_12@yahoo.com>
2020-04-05 11:48:34 -0400

    fix indentation

--- 2020/ParentingPartnering.java
+++ 2020/ParentingPartnering.java
@@ -21,8 +21,8 @@ public class ParentingPartnering {
 		  }
 		  if (currentTask.get('J') != null) {
 			  if (sortedIntervals[pointer][0] >= currentTask.get('J')){
-		    	  currentTask.remove('J');  
-		      }
+				  currentTask.remove('J');  
+			  }
 			  
 		  }
 		
@@ -51,33 +51,31 @@ public class ParentingPartnering {
 		
 		for(int j=0; j<intervals.length; j++) {
 			ArrayList<Integer> time = new ArrayList<Integer>();
-		    time.add(intervals[j][0]);
-		    time.add(intervals[j][1]);
-		    
+			time.add(intervals[j][0]);
+			time.add(intervals[j][1]);
+			
 			if(finalSchedule.get(time) != null) {
-		    	parentSchedule += 'C';
-		    	finalSchedule.remove(time);
+				parentSchedule += 'C';
+				finalSchedule.remove(time);
 			}
 			else {
 				parentSchedule += 'J';
-		    }
-		  
-		 }
+			}
+		}
 		
 		return parentSchedule;
 	}
     
     public static void sortByStartTime(int arr[][]) 
     { 
-        Arrays.sort(arr, new Comparator<int[]>() { 
-            
-          public int compare(final int[] entry1, final int[] entry2) { 
-            if (entry1[0] > entry2[0]) 
-                return 1; 
-            else
-                return -1; 
-          } 
-        });
+		Arrays.sort(arr, new Comparator<int[]>() { 
+			public int compare(final int[] entry1, final int[] entry2) { 
+				if (entry1[0] > entry2[0]) 
+					return 1; 
+				else
+					return -1; 
+			} 
+		});
     } 
 
 	public static void main(String[] args) {
@@ -88,16 +86,15 @@ public class ParentingPartnering {
 			int[][] matrix = new int[n][2];
 			for(int j=0; j<n; j++) {
 				for(int k=0; k<2; k++) {
-				matrix[j][k] = in.nextInt();
+					matrix[j][k] = in.nextInt();
 				}
 			}
-	      
+		  
 			String schedule = assignSchedule(matrix);
 			  
 			System.out.println("Case #" + i + ": " + schedule);
 		}
 
 	}
-
 
 }
