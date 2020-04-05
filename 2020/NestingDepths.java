3a46d43a8b2cd7384c4a4f470d99c48b98176ab6
Alvina Putri <av_ashfan_12@yahoo.com>
2020-04-05 11:42:32 -0400

    fix indentation

--- 2020/NestingDepths.java
+++ 2020/NestingDepths.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*;
 
 public class NestingDepths {
-
+	
 	public static void addBrackets(Stack<Character> parenthesis, char num, int diff) {
 		int n = Character.getNumericValue(num);
 		for(int i = 0; i<diff; i++) {
@@ -12,7 +12,6 @@ public class NestingDepths {
 		for(int i = 0; i<n; i++) {
 			parenthesis.add(')');
 		}
-		
 	}
 	
 	public static void insertDigit(Stack<Character> parenthesis, char num) {
@@ -27,42 +26,42 @@ public class NestingDepths {
 	}
 	
 	public static void printStack(Stack<Character> parenthesis) {  
-	    if (parenthesis.isEmpty())  
-	        return;  
-	      
-	    char top = parenthesis.peek();  
-	    parenthesis.pop();  
-	  
-	    printStack(parenthesis);  
-	    System.out.print(top);  
-	    parenthesis.push(top);  
+		if (parenthesis.isEmpty())  
+			return;  
+
+		char top = parenthesis.peek();  
+		parenthesis.pop();  
+
+		printStack(parenthesis);  
+		System.out.print(top);  
+		parenthesis.push(top);  
 	}  
 	
 	
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
-	    int t = in.nextInt();
-	    for (int i = 1; i <= t; ++i) {
-	    	String nums = in.next();
-	    	Stack<Character> parenthesis = new Stack<>();
-	    	int depth = 0;
+		int t = in.nextInt();
+		for (int i = 1; i <= t; ++i) {
+			String nums = in.next();
+			Stack<Character> parenthesis = new Stack<>();
+			int depth = 0;
 			for(int j = 0; j < nums.length(); j++) {
 				char n = nums.charAt(j);
 				int num = Character.getNumericValue(n);
 				if(depth < num) {
 					int diff = num-depth;
-				    if (parenthesis.isEmpty()) {
-				    	addBrackets(parenthesis, n, diff);
-				    }
-				    else {
-						char top = parenthesis.peek(); 
-					    while(top == ')') {
-					    	parenthesis.pop();  
-						    top = parenthesis.peek(); 
-					    }
+					if (parenthesis.isEmpty()) {
 						addBrackets(parenthesis, n, diff);
-				    }
+					}
+					else {
+						char top = parenthesis.peek(); 
+						while(top == ')') {
+							parenthesis.pop();  
+							top = parenthesis.peek(); 
+						}
+						addBrackets(parenthesis, n, diff);
+					}
 				}
 				else {
 					insertDigit(parenthesis, n);
@@ -72,10 +71,7 @@ public class NestingDepths {
 			System.out.print("Case #" + i + ": ");
 			printStack(parenthesis);
 			System.out.println();
-			
-	    }
-		
+		}
 	}
-
 
 }
